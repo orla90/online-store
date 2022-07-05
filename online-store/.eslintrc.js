e66c9dca8d7d572fedd42ec34ded1b636872ce5f
orla90 <orla90@inbox.ru>
2022-07-05 11:35:22 +0300

    init: start online-store task

---
+++ online-store/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    root: true,
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    extends: [
+      'airbnb-typescript/base',
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      "plugin:@typescript-eslint/recommended-requiring-type-checking"
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: './tsconfig.json',
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: [
+      '@typescript-eslint',
+      'import'
+    ],
+    rules: {
+      "semi": ["error", "always"],
+      "@typescript-eslint/no-explicit-any": 2,
+    },
+  };
