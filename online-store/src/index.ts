04154ff4e59225ee63e2b4e6d8207c6c1fb95deb
orla90 <orla90@inbox.ru>
2022-07-11 13:07:18 +0300

    feat: add isPopular filter

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -1,18 +1,20 @@
-import { Card } from './ts/сard'; 
-import { data } from './ts/Data';
-import { CardData } from './ts/Interface';
+import { Card } from "./ts/сard";
+import { data } from "./ts/Data";
+import { CardData } from "./ts/Interface";
 
 window.onload = function () {
   console.log("Hello");
 
   // Render Cards
-  if(data) {
+  if (data) {
     renderCardsToDom();
   }
 
-  //Filters
+  //Filters handlers
   addManufacturerClickHandler();
   addCamerasClickHandler();
+  addColorsClickHandler();
+  addIsPopularClickHandler();
 };
 
 const addManufacturerClickHandler = () => {
@@ -29,18 +31,28 @@ const addManufacturerClickHandler = () => {
 };
 
 const addCamerasClickHandler = () => {
-  document
-    .querySelector(".camera-amount")
-    ?.addEventListener("click", (e) => {
-      if (
-        (e.target as HTMLDivElement).classList.contains("button_camera")
-      ) {
-        let clickedCameraFilter = e.target;
-        selectClickedCameraFilter(clickedCameraFilter);
-      }
-    });
-}
+  document.querySelector(".camera-amount")?.addEventListener("click", (e) => {
+    if ((e.target as HTMLDivElement).classList.contains("button_camera")) {
+      let clickedCameraFilter = e.target;
+      selectClickedCameraFilter(clickedCameraFilter);
+    }
+  });
+};
 
+const addColorsClickHandler = () => {
+  document.querySelector(".colors")?.addEventListener("click", (e) => {
+    if ((e.target as HTMLDivElement).classList.contains("button_color")) {
+      let clickedColorFilter = e.target;
+      selectClickedColorFilter(clickedColorFilter);
+    }
+  });
+};
+
+const addIsPopularClickHandler = () => {
+  document.querySelector("#popular-input")?.addEventListener("click", () => {
+    filterCardsByManufacturer();
+  });
+};
 
 const selectClickedManufacturerFilter = (
   clickedManufacturerFilter: EventTarget | null
@@ -52,8 +64,12 @@ const selectClickedManufacturerFilter = (
 const selectClickedCameraFilter = (clickedCameraFilter: EventTarget | null) => {
   (clickedCameraFilter as HTMLDivElement).classList.toggle("active");
   filterCardsByManufacturer();
-}
+};
 
+const selectClickedColorFilter = (clickedColorFilter: EventTarget | null) => {
+  (clickedColorFilter as HTMLDivElement).classList.toggle("active");
+  filterCardsByManufacturer();
+};
 
 const filterCardsByManufacturer = () => {
   const cards = document.querySelectorAll(
@@ -68,38 +84,42 @@ const filterCardsByManufacturer = () => {
 
   if (activeManufacturerButtons.length !== 0) {
     cards.forEach((card) => {
-    card.classList.add("store-content__item_hidden");
-  });
+      card.classList.add("store-content__item_hidden");
+    });
 
-  manufacturerButtons.forEach((manufacturerButton) => {
-    if (manufacturerButton.classList.contains("active")) {
-      cards.forEach((card) => {
-        let manufacturerProp = (card.querySelector(
-          ".manufacturer-prop"
-        ) as HTMLDivElement).innerText;
-        if (
-          (manufacturerButton as HTMLDivElement).innerText === manufacturerProp
-        ) {
-          card.classList.remove("store-content__item_hidden");
-        }
-      });
-    }
-  });
+    manufacturerButtons.forEach((manufacturerButton) => {
+      if (manufacturerButton.classList.contains("active")) {
+        cards.forEach((card) => {
+          let manufacturerProp = (card.querySelector(
+            ".manufacturer-prop"
+          ) as HTMLDivElement).innerText;
+          if (
+            (manufacturerButton as HTMLDivElement).innerText ===
+            manufacturerProp
+          ) {
+            card.classList.remove("store-content__item_hidden");
+          }
+        });
+      }
+    });
 
-  const cardsFilteredByManufacturer = Array.from(cards).filter(card => !card.classList.contains("store-content__item_hidden"));
-  console.log(cardsFilteredByManufacturer);
-  filterCardsByCameras(cardsFilteredByManufacturer); 
-
+    const cardsFilteredByManufacturer = Array.from(cards).filter(
+      (card) => !card.classList.contains("store-content__item_hidden")
+    );
+    filterCardsByCameras(cardsFilteredByManufacturer);
   } else {
-    console.log(cards);
     cards.forEach((card) => {
       card.classList.remove("store-content__item_hidden");
     });
-    filterCardsByCameras(Array.from(cards)); 
+    filterCardsByCameras(Array.from(cards));
   }
 };
 
 const filterCardsByCameras = (cardsArr: Array<Element>) => {
+  const cards = document.querySelectorAll(
+    ".layout-5-column .store-content__item"
+  );
+
   const camerasButtons = document.querySelectorAll(
     ".camera-amount .button_camera"
   );
@@ -110,46 +130,112 @@ const filterCardsByCameras = (cardsArr: Array<Element>) => {
 
   if (activeCameraButtons.length !== 0) {
     cardsArr.forEach((card) => {
-    if (!card.classList.contains("store-content__item_hidden")) {
-      card.classList.add("store-content__item_hidden");
-    }
-  });
+      if (!card.classList.contains("store-content__item_hidden")) {
+        card.classList.add("store-content__item_hidden");
+      }
+    });
 
-  camerasButtons.forEach((cameraButton) => {
-    if (cameraButton.classList.contains("active")) {
-      cardsArr.forEach((card) => {
-        let cameraProp = (card.querySelector(
-          ".camera-prop"
-        ) as HTMLDivElement).innerText;
-        if (
-          (cameraButton as HTMLDivElement).innerText === cameraProp
-        ) {
-          card.classList.remove("store-content__item_hidden");
-        }
-      });
-    }
-  });
+    camerasButtons.forEach((cameraButton) => {
+      if (cameraButton.classList.contains("active")) {
+        cardsArr.forEach((card) => {
+          let cameraProp = (card.querySelector(
+            ".camera-prop"
+          ) as HTMLDivElement).innerText;
+          if ((cameraButton as HTMLDivElement).innerText === cameraProp) {
+            card.classList.remove("store-content__item_hidden");
+          }
+        });
+      }
+    });
+
+    const cardsFilteredByCameras = Array.from(cards).filter(
+      (card) => !card.classList.contains("store-content__item_hidden")
+    );
+    filterCardsByColors(cardsFilteredByCameras);
+  } else {
+    filterCardsByColors(cardsArr);
+  }
 };
 
-}
+const filterCardsByColors = (cardsArr: Array<Element>) => {
+  const cards = document.querySelectorAll(
+    ".layout-5-column .store-content__item"
+  );
+
+  const colorsButtons = document.querySelectorAll(".colors .button_color");
+
+  const activeColorsButtons = document.querySelectorAll(
+    ".colors .button_color.active"
+  );
+
+  if (activeColorsButtons.length !== 0) {
+    cardsArr.forEach((card) => {
+      if (!card.classList.contains("store-content__item_hidden")) {
+        card.classList.add("store-content__item_hidden");
+      }
+    });
+
+    colorsButtons.forEach((colorsButton) => {
+      if (colorsButton.classList.contains("active")) {
+        cardsArr.forEach((card) => {
+          let colorProp = (card.querySelector(".color-prop") as HTMLDivElement)
+            .innerText;
+          if ((colorsButton as HTMLDivElement).innerText === colorProp) {
+            card.classList.remove("store-content__item_hidden");
+          }
+        });
+      }
+    });
+
+    const cardsFilteredByColors = Array.from(cards).filter(
+      (card) => !card.classList.contains("store-content__item_hidden")
+    );
+    filterCardsByIsPopular(cardsFilteredByColors);
+  } else {
+    filterCardsByIsPopular(cardsArr);
+  }
+};
+
+const filterCardsByIsPopular = (cardsArr: Array<Element>) => {
+  const isPopularButtonChecked = document.querySelectorAll(
+    "#popular-input:checked"
+  );
+
+  if (isPopularButtonChecked.length !== 0) {
+    cardsArr.forEach((card) => {
+      if (!card.classList.contains("store-content__item_hidden")) {
+        card.classList.add("store-content__item_hidden");
+      }
+    });
+
+    cardsArr.forEach((card) => {
+      let isPopularProp = (card.querySelector(
+        ".popular-prop"
+      ) as HTMLDivElement).innerText;
+      if (isPopularProp === "да") {
+        card.classList.remove("store-content__item_hidden");
+      }
+    });
+  }
+};
 
 const renderCardsToDom = () => {
   let cardsWrapper = getCardsWrapper();
-  generateCards(data).forEach(card => {
-    cardsWrapper?.append(card.generateCard())
-  })
+  generateCards(data).forEach((card) => {
+    cardsWrapper?.append(card.generateCard());
+  });
 };
 
 const getCardsWrapper = () => {
   const cardsContainer = document.querySelector(".store-content__wrapper");
   (cardsContainer as HTMLDivElement).innerHTML = "";
   return cardsContainer;
-}
+};
 
 const generateCards = (data: Array<CardData>) => {
   let cards: Card[] = [];
-  data.forEach(card => {
+  data.forEach((card) => {
     cards.push(new Card(card));
   });
   return cards;
-}
+};
